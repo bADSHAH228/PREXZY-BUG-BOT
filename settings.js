f16b5510227bf78fdedb6006387026f9b778afd4
bADSHAH228 <rb6767491@gmail.com>
2024-09-22 23:07:25 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,19 +2,19 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
+global.ownernomer "923271656076"
 global.ownername = "Precious Ayomide👑"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
+global.ownernumber = '923271656076'  //creator number
 global.ownername = 'Precious Ayomide👑' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
-global.author = 'PREXZY👑\n\nContact: +263717750825'
+global.author = 'PREXZY👑\n\nContact: +923271656076'
 
 //console view/theme
 global.themeemoji = '🪀'
